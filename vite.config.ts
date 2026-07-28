7062afe528c9ec0c511147ae21117a2de4905512
JamesYew26 <junchee0206@gmail.com>
2026-07-29 01:49:15 +0800

    chore: general update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss()],
-
-})
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'My custom React PWA',
+        short_name: 'ReactPWA',
+        description: 'A slick React PWA with custom UI interactions',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'wegame-h5-share.f036f35a.webp',
+            sizes: '192x192',
+            type: 'image/webp'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
